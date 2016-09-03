e8c2358da8fdb57a9d41c5919c2e6db955af58be
Siri Khalsa <skhalsa10@live.com>
2016-09-03 16:43:04 -0600

    making final version of file renamed original to old and created backup pushing to compile on schoole computer

---
+++ lab02/wordCountold.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define TRUE 1 //will be used for my own boolean value
+#define FALSE 0 // used as boolean
+
+/*********************************************
+*                                            *
+* This program will echo what is fed into it.*
+* While it echos it it counds words and lines*
+*                                            *
+**********************************************/
+
+main()
+{
+  int c, numOfChars, numOfLines, numOfWordsTotal, isNewLine, inWord;
+  int numOfWords, numOfCharsTotal;
+  numOfChars = numOfLines = numOfCharsTotal = 0;
+  numOfWordsTotal = numOfWords = 1;
+  isNewLine = inWord = TRUE;
+
+  while((c = getchar()) != EOF)
+  {
+    numOfCharsTotal++;
+    numOfChars++;
+    if(isNewLine == TRUE)
+    {
+      numOfLines++;
+      isNewLine = FALSE;
+      printf("%d.", numOfLines);
+    }
+    if(c == ' ' || c == '\n' || c == '\t')
+    {
+      inWord = FALSE;
+      if (c == '\n')
+      {
+        printf("[%d,%d]", numOfWords, numOfChars);
+        numOfChars = 1;
+        numOfWords = 1;
+        isNewLine = TRUE;
+      }
+    }
+    else if(inWord == FALSE)
+    {
+      inWord = TRUE;
+      numOfWords++;
+      numOfWordsTotal++;
+    }
+    if(isNewLine == TRUE) printf("[%d, %d]", numOfWords, numOfChars);
+    putchar(c);
+  }
+
+  printf("%d of lines, %d of words, %d of characters\n", numOfLines, numOfWordsTotal, numOfCharsTotal );
+
+
+}
