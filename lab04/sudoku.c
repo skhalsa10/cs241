08126520b67a6ac62b683d46b5dc32efcb5c5c10
Siri Khalsa <skhalsa10@live.com>
2016-09-19 11:59:45 -0600

    compiled successfully

--- lab04/sudoku.c
+++ lab04/sudoku.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 /* 2d array ill call theGrid */
-int theGrid[9][9]
+int theGrid[9][9];
 /* declare variable for charStream*/
 int charStream = 0;
 
 int main()
 {
+  
   /*the program continues until the EOF characters
   it will copy 81 numbers and blanks int a 2d array.
   if it comes accross a non number character then the
   error flag is turned on. if there are not 81 characters
   '\n' error flag turned on. if there are more then 81
   characters before '\n' error flag is turned on. */
-   while((charStream != EOF)
+   while(charStream != EOF)
    {
      int i = 0;
      int j = 0;
