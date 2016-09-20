177b0073b29c1825f08b48cca2d071ec3466e08c
Siri Khalsa <skhalsa10@live.com>
2016-09-19 20:47:31 -0600

    move to atom editor

--- lab04/sudoku.c
+++ lab04/sudoku.c
@@ -14,19 +14,38 @@ int main()
   error flag is turned on. if there are not 81 characters
   '\n' error flag turned on. if there are more then 81
   characters before '\n' error flag is turned on. */
-   while(charStream != EOF)
+  while((charStream = getchar()) != EOF)
    {
-     int i = 0;
-     int j = 0;
+       int i = 0;
+       int j = 0;
      for(i = 0; i < 9; i++)
      {
        for (j = 0; j < 9; j++)
        {
-         charStream = getchar();
-         putchar(charStream);
-         theGrid[i][j] = charStream;
+	 if(charStream == '.')
+	   {
+	     theGrid[i][j] = 0;
+	   }
+	 else if ((charStream >= '0' ) && (charStream <= '9'))
+	   {
+	     theGrid[i][j] = (charStream - '0');
+	   }
+	 charStream = getchar();
        }
-       printf("\n" );
      }
+     charStream = getchar();
+     for(i=0; i<9;i++)
+       {
+	 printf("\n+---------------------------+\n");
+	 for(j=0;j<9;j++)
+	   {
+	     printf("|%d|", theGrid[i][j]);
+	   }
+       }
    }
+}
+
+int convertLineToGrid()
+{
+
 }
