f5cfcfac71624226aebfbaa728ec925c0a87c146
Siri Khalsa <skhalsa10@live.com>
2016-09-19 22:29:35 -0600

    confirming error checking is working

--- lab04/sudoku.c
+++ lab04/sudoku.c
@@ -2,6 +2,9 @@
 #define FALSE 0
 #define TRUE 1
 
+/* declare all functions here */
+
+
 /* 2d array ill call theGrid */
 int theGrid[9][9];
 /* declare variable for charStream*/
@@ -23,8 +26,7 @@ int main()
   while((charStream = getchar()) != EOF)
   {
     convertLineToGrid();
-    int i = 0;
-    int j = 0;
+    checkIfGridLegal()
     if(errorNotNumeric)
     {
       printf("errorNotNumeric\n");
@@ -54,6 +56,7 @@ int convertLineToGrid()
 
   /* loop over line and copt to theGrid*/
   int i = 0;
+  int count = 1;
   int j = 0;
   for(i = 0; i < 9; i++)
   {
@@ -70,20 +73,27 @@ int convertLineToGrid()
       /*test for line to short error */
       else if (charStream == '\n')
       {
+        printf("breaking at line to short%d\n", count);
         errorLineTooShort = TRUE;
         break;
       }
       /*if it makes it this far than we have nonnumeric char*/
       else
       {
+        printf("breaking at nonnumeric%d\n", count);
         errorNotNumeric = TRUE;
         break;
       }
+      count++;
       charStream = getchar();
     }
     /* if we broke out of the first loop due
      to error just break the second one */
-    if (errorNotNumeric||errorLineTooShort) break;
+    if (errorNotNumeric||errorLineTooShort)
+    {
+      printf("breaking immediately after error%d\n", count)
+      break;
+    }
   }
   /* if we get 81 characters the next char should be'\n'
   but if our line is too short this will keep the stream in order
