1cc5b3e89c0cbf0908517e4bfbb13dd91fdb5f29
Siri Khalsa <skhalsa10@live.com>
2016-09-25 21:37:58 -0600

    rewrote complexSolution function

--- lab04/sudoku.c
+++ lab04/sudoku.c
@@ -357,11 +357,12 @@ void printConstraintGrid()
   int j = 0;
   for(i=0;i<9;i++)
   {
+    printf("\n---------------------------\n");
     for(j=0;j<9;j++)
     {
-      printf("%3d|\n", constraintGrid[i][j]);
+      printf("%3d|", constraintGrid[i][j]);
     }
-    printf("---------------------------\n");
+
   }
 }
 
