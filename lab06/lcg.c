bb4dc5b08c73d7f0fbc800dcd7ad406041e5a777
Siri Khalsa <skhalsa10@live.com>
2016-10-28 22:38:03 -0600

    c per spec can be  but supposidly cannot be 0... 0 does seem to break everything so I will get rid of it

--- lab06/lcg.c
+++ lab06/lcg.c
@@ -13,7 +13,7 @@ LCG makeLCG(u64 m, u64 c)
   lcg.c = c;
   lcg.a = (m%4 == 0) ? (1+2*calculateP(m)):(1+calculateP(m)) ;
 
-  if(lcg.c >= lcg.m || lcg.a >= lcg.m || lcg.a <= 0 || lcg.m <= 0 || lcg.c < 0)
+  if(lcg.c >= lcg.m || lcg.a >= lcg.m || lcg.a <= 0 || lcg.m <= 0 || lcg.c <= 0)
   {
     lcg.x = 0;
     lcg.m = 0;
