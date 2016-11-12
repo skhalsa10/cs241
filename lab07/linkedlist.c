007600b9f773e0095639f18438c381f5693b9b3a
Siri Khalsa <skhalsa10@live.com>
2016-11-12 12:42:56 -0700

    forgot ;

--- lab07/linkedlist.c
+++ lab07/linkedlist.c
@@ -204,7 +204,7 @@ void freeList(struct ListNode* head)
  ************************************************************/
 void reverseList(struct ListNode** headRef)
 {
-  printList(*headRef)
+  printList(*headRef);
   struct ListNode** currentRef = headRef;
   while((*currentRef != NULL))
   {
