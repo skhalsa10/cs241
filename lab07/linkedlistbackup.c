bf57fdf2aba42b420b6b6f36611c014819be0a1d
Siri Khalsa <skhalsa10@live.com>
2016-11-13 17:13:40 -0700

    mustfixlist!!!

--- lab07/linkedlistbackup.c
+++ lab07/linkedlistbackup.c
@@ -204,7 +204,11 @@ void freeList(struct ListNode* head)
  ************************************************************/
 void reverseList(struct ListNode** headRef)
 {
-
+  struct ListNode** currentRef = headRef;
+  if((*currentRef)->next == NULL) return;
+  swapFirstLastNode(currentRef);
+  currentRef = &((*headRef)->next);
+  reverseList(currentRef);
 }
 
 /*************************************************************
