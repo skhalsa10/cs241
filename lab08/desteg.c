9be5fcf891a0ab0f1db457fb4acbd24f3a7b037a
Siri Khalsa <skhalsa10@live.com>
2016-11-19 22:40:56 -0700

    what?

--- lab08/desteg.c
+++ lab08/desteg.c
@@ -17,6 +17,7 @@ int main(int argc, char** argv)
     char* inFileName = argv[1];
     int i;
     char code;
+    int finished = 0;
     unsigned char bytes[4];
 
     FILE* in = fopen(inFileName, "rb");
@@ -27,7 +28,7 @@ int main(int argc, char** argv)
         getc(in);
     }
 
-    while(code != NULL)
+    while(code != 0)
     {
         bytes[0]= 0;
         bytes[1]= 0;
@@ -40,7 +41,7 @@ int main(int argc, char** argv)
         code = (code<<2)| (bytes[2]&3);
         code = (code<<2)| (bytes[3]&3);
 
-        if(code != NULL) putchar(code);
+        if(code != 0) putchar(code);
     }
 
     fclose(in);
