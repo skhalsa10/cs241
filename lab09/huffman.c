b8c8b5c47e920d441b373ae3e8da8df573fef7cb
Siri Khalsa <skhalsa10@live.com>
2016-11-25 10:10:29 -0700

    updated array to only be 260 chars

--- lab09/huffman.c
+++ lab09/huffman.c
@@ -4,10 +4,10 @@
 /*encodes file*/
 void encodeFile(FILE* in, FILE* out)
 {
-  unsigned int freqCounter[500];
+  unsigned int freqCounter[260];
   int i;
   /*initialize array to 0*/
-  for(i=0;i<500;i++)
+  for(i=0;i<260;i++)
   {
     freqCounter[i] = 0;
   }
@@ -29,7 +29,7 @@ void printFreq(unsigned int freqCounter[])
   int i;
   unsigned long totalChars = 0;
   printf("Symbol\tFreq\n");
-  for(i=0;i<500;i++)
+  for(i=0;i<260;i++)
   {
     if(freqCounter[i] != 0)
     {
