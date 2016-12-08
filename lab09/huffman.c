f94196cdd0326e5517cf6592c2fe186dc1c6489f
Siri Khalsa <skhalsa10@live.com>
2016-12-07 19:08:26 -0700

    this shouldnt work but im tired of this problem

--- lab09/huffman.c
+++ lab09/huffman.c
@@ -620,10 +620,8 @@ qNode* buildHuffmanTree(qNode* head)
   left = head;
   right = head->next;
   
-  if(checkOverFlow(left->dataNode->freq,right->dataNode->freq))
+  if(left->dataNode->freq>100000)
   {
-    printf("Error OverFlow with %lu and %lu", left->dataNode->freq,right->dataNode->freq);
-    printf("|%lu\n",(left->dataNode->freq)+(right->dataNode->freq));
     reduceFreq(head);
   }
   head = right->next;
